f7f08983336a67b6dfc2a004c0eae1c10227fe7e
kisurekeyvick <zttaijue1207@163.com>
2020-04-26 16:14:40 +0800

    feat(src/environments/environment.prod.ts):修改环境

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  SERVER_URL: `${window.location.host}/`, // 'http://281e42459f.zicp.vip/',
+  SERVER_URL: `http://281e42459f.zicp.vip:23081/`, // 'http://281e42459f.zicp.vip/',
   appCode: '',
   envType: 'prod'
 };
